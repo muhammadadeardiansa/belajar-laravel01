a607738bee5c3bd58db2e2e8baf8b7747558e28f
muhammadadeardiansa <ade18121003@gmail.com>
2021-10-18 20:53:50 +0700

    menambahkan content

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.master');
+});
+
+Route::get('/test', function () {
+    return view('tescontent');
+});
+
+Route::get('/test2', function () {
+    return view('testcontent2');
 });
